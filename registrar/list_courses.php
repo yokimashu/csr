560089e12525bf8029842c2acb3908495d6592b5
pinkstel <khristelculi@gmail.com>
2021-01-25 21:34:16 +0800

    Update list_courses.php

--- registrar/list_courses.php
+++ registrar/list_courses.php
@@ -74,7 +74,7 @@ include('../includes/sidebar.php');
                 <thead>
                   <tr>
                     <th> Course</th>
-                    <th> Number of Enrolled </th>
+                    <th> Number of Enrolles </th>
                   </tr>
                 </thead>
                 <tbody>
