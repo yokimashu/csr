2dec61b091c50df702b8b7275b974a862b5d6366
pinkstel <khristelculi@gmail.com>
2021-03-18 21:49:35 +0800

    Update add_faculty.php

--- registrar/add_faculty.php
+++ registrar/add_faculty.php
@@ -107,18 +107,20 @@ include('../includes/sidebar.php');
                         </div>
                       </div>
 
+
                       <div class="control-group">
-                      <label class="control-label" for="multiSelect">Department/s:</label>
-                      <div class="controls">
-                        <select multiple="multiple" id="multiSelect" class="chzn-select span5" name="courses[]">
-                          <option>
-                            <?php while ($courses = $get_all_courses_data->fetch(PDO::FETCH_ASSOC)) { ?>
-                          <option value="<?php echo
-                                          $get_courses['courses']; ?>"><?php echo $get_courses['courses']; ?></option>
-                        <?php } ?>
-                        </select>
-                        <p class="help-block">Start typing to activate auto complete!</p>
-                      </div>
+  								<label class="control-label">Department: <span class="required">*</span></label>
+  								<div class="controls">
+  									<select class="span6 m-wrap" id ="gender" name="category">
+  										<option value="">Select...</option>
+  										<option value="BSIT Department">BSIT Department</option>
+  										<option value="BSBA Department">BSBA Department</option>
+                      <option value="BEED Department">BEED Department</option>
+  										<option value="Midwiferey Department">Midwiferey Department</option>
+                      <option value="Tourism Department">Tourism Department</option>
+  									</select>
+  								</div>
+                </div>
 
 
                       <div class="control-group">
