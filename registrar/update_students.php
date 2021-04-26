7ab829ed0f2b7ec3d303ab2f5c1f62f22e90b256
jonardmondero <62581989+jonardmondero@users.noreply.github.com>
2021-04-27 06:38:19 +0800

    update

---
+++ registrar/update_students.php
@@ -0,0 +1,146 @@
+<?php
+include('../config/db_config.php');
+date_default_timezone_set('Asia/Manila');
+
+$alert_msg = '';     
+
+//if button insert clicked
+
+if (isset($_POST['students_id'])) {
+
+  
+  //     echo "<pre>";
+  //     print_r($_POST);
+  // echo "</pre>";
+
+  $students_id = $_POST['students_id'];
+  $first_name = $_POST['first_name'];
+  $middle_name = $_POST['middle_name'];
+  $last_name = $_POST['last_name'];
+  $date_of_birth = date('Y-m-d', strtotime($_POST['date_of_birth']));
+  $place_of_birth = $_POST['place_of_birth'];
+  $nationality = $_POST['nationality'];
+  $gender = $_POST['gender'];
+  $status = $_POST['status'];
+  $contact_number   = $_POST['contact_number'];
+  $facebook_account = $_POST['facebook_account'];
+  $religion   = $_POST['religion'];
+  $baptized  = $_POST['baptized'];
+  $confirmed = $_POST['confirmed'];
+  $elementary_school = $_POST['elemSchool'];
+  $high_school = $_POST['highSchool'];
+  $last_attended_college = $_POST['college'];
+  $street = $_POST['street'];
+  $subd = $_POST['subd'];
+  $brgy = $_POST['brgy'];
+  $city = $_POST['city'];
+  $province = $_POST['province'];
+  $region = $_POST['region'];
+  $zip_code = $_POST['zip_code'];
+    $parent_name  =  $_POST['parentName'];
+      $parent_contact_number   = $_POST['parentContactNumber'];
+      $parent_address  =  $_POST['parentAddress'];
+      $parent_occupation  =  $_POST['parentOccupation'];
+      $fileName = 'default.jpg';
+      $img = $_POST['studentimage'];
+    //insert user to database
+    $register_user_sql      = "CALL spUpdateStudent(
+      :students_id,
+    :first_name,
+    :middle_name,
+    :last_name,
+    :date_of_birth,
+    :place_of_birth,
+    :nationality,
+    :gender,
+    :status,
+    :contact_number,
+    :facebook_account,
+    :religion,
+    :baptized,
+    :confirmed,
+    :elementary_school,
+    :high_school,
+    :last_attended_college,
+    :street,
+    :vil_sub,
+    :barangay,
+    :city,
+    :province,
+    :region,
+    :zip_code,
+    :parent,
+    :address,
+    :contact,
+    :occupation,
+    :photo
+   )";
+
+    $register_data = $con->prepare($register_user_sql);
+    $register_data->execute([
+      ':students_id'           => $students_id,
+      ':first_name'            => $first_name,
+      ':middle_name'           => $middle_name,
+      ':last_name'             => $last_name,
+      ':date_of_birth'         => $date_of_birth,
+      ':place_of_birth'        => $place_of_birth,
+      ':nationality'           => $nationality,
+      ':gender'                => $gender,
+      ':status'                => $status,
+      ':contact_number'        => $contact_number,
+      ':facebook_account'      => $facebook_account,
+      ':religion'              => $religion,
+      ':baptized'              => $baptized,
+      ':confirmed'             => $confirmed,
+      ':elementary_school'     => $elementary_school,
+      ':high_school'           => $high_school,
+      ':last_attended_college' => $last_attended_college,
+      ':street'         =>      $street,
+      ':vil_subd'      =>      $subd,
+      ':barangay'      =>      $brgy,
+      ':city'          =>      $city,
+      ':province'      =>      $province,
+      ':region'        =>      $region,
+      ':zip_code'      =>      $zip_code,
+      ':parent'             =>      $parent_name,
+      ':address'            =>      $parent_address,
+      ':contact'            =>      $parent_contact_number,
+      ':occupation'         =>      $parent_occupation,
+      ':photo'              =>      $img
+    ]);
+ 
+
+  if($img !=  '')
+  {
+    unlink('../studentimage/' . $img);
+  $folderPath = "../studentimage/";
+  $image_parts = explode(";base64,", $img);
+  $image_type_aux = explode("image/", $image_parts[0]);
+  $image_type = $image_type_aux[1];
+  $image_base64 = base64_decode($image_parts[1]);
+  $fileName = uniqid() . '.jpg';
+  $file = $folderPath . $fileName;
+  file_put_contents($file, $image_base64);
+
+  }
+  $add_photo = "UPDATE student_image SET 
+  photo = :photo 
+  WHERE student_id = :student_id";
+
+  $execute_photo = $con->prepare($add_photo);
+  $execute_photo->execute([
+    ':student_id'   =>  $students_id,
+    ':photo'        =>  $fileName
+  ]);
+
+}
+  
+
+
+
+
+
+
+
+
+?>
